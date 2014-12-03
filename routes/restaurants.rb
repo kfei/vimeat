f52e1b8ae8b4db99a37ed0491ca6c218a1dbc771
kfei <kfei@kfei.net>
2014-12-03 10:41:42 +0800

    Remove two example APIs in code
    1) restaurant's key deletion
    2) convert all restaurant's images to list
    
    Signed-off-by: kfei <kfei@kfei.net>

--- routes/restaurants.rb
+++ routes/restaurants.rb
@@ -22,28 +22,4 @@ class VimEat < Sinatra::Base
 		update_json('restaurant', tmp)
 		200
 	end
-
-	# Method to delete a key from the restaurants hash
-	# delete '/restaurants/key' do
-	# 	f = get_json_all('restaurant')
-	# 	f = JSON.parse(f)
-	# 	tmp = {}
-	# 	tmp['restaurants'] = []
-
-	# 	f['restaurants'].each do |r|
-	# 		tmp['restaurants'].push(r.delete_if { |key, value| key == '<key_name>'})
-	# 	end
-	# 	update_json('restaurant', tmp)
-	# 	200
-	# end
-
-	# Convert all restaurant.img to array
-	# get '/restaurants/array' do
-	# 	j = JSON.parse(get_json_all('restaurant'))
-	# 	j['restaurants'].each do |r|
-	# 		arr = [r['img']]
-	# 		r['img'] = arr
-	# 	end
-	# 	update_json('restaurant', j)
-	# end
 end
